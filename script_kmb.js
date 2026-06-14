ab8e231a8debdf56173708775d3dbc95a8708d32
GOPSTNM <166233545+GOPSTNM@users.noreply.github.com>
2026-06-14 22:47:08 +0800

    Refined Alg for Names and Storage (1)

--- script_kmb.js
+++ script_kmb.js
@@ -1,4 +1,26 @@
 const api_base = "https://data.etabus.gov.hk/v1/transport/kmb";
+const name_word_replacements_en = {
+    "BBI": "BBI",
+    "Ground Transportation Centre": "GTC",
+    "GTC": "GTC",
+    "II": "II",
+    "HKCECE": "Exhibition Centre",
+    "HK": "HK",
+    "H.K.": "HK",
+    "O'Brien": "O'Brien",
+    "EKCC": "EKCC",
+    "HACTL": "HACTL",
+    "JPC": "JPC",
+}
+const name_escaped_keys_en = Object.keys(name_word_replacements_en).map(key => key.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&"));
+const name_replacements_regex_en = new RegExp(`(?<=^|\\s|\\(|-)${name_escaped_keys_en.join("|")}(?=\\s|\\)|-|$)`, "gi");
+const name_word_replacements_tc = {
+    "II": "II"
+}
+const name_escaped_keys_tc = Object.keys(name_word_replacements_tc).map(key => key.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&"));
+const name_replacements_regex_tc = new RegExp(`(?<=^|\\s|\\(|-)${name_escaped_keys_tc.join("|")}(?=\\s|\\)|-|$)`, "gi");
+const name_pole_id_regex = /\s\(([A-Z]{2}\d{3}[A-Z]?)\)$/i;
+const name_title_case_regex = /(?<=^|[\s\-()\/])[a-z]/g;
 let route_data = {};
 let eta_data = {};
 let route_terminus_index = {};
@@ -14,6 +36,34 @@ async function setup() {
         document.getElementById("rte_options_select").value = url_params.get("d");
         update();
     }
+}
+function proper_stop_name_id(name) {
+    const pole_id = name.match(name_pole_id_regex);
+    return pole_id ? pole_id[1] : "";
+}
+function proper_stop_name_en(name) {
+    const stop_name = name.replace(name_pole_id_regex, "").trim().replace(/(?<!\s)\(/g, " (").replace(/,(?!\s)/g, ", ");
+    let proper_stop_name = stop_name.toLowerCase().replace(name_title_case_regex, (txt) => {
+        return txt.toUpperCase();
+    });
+    return proper_stop_name.replace(name_replacements_regex_en, (matched) => {
+        const name_replace_key = Object.keys(name_word_replacements_en).find(
+            key => key.toLowerCase() === matched.toLowerCase()
+        );
+        return name_word_replacements_en[name_replace_key] || matched;
+    });
+}
+function proper_stop_name_tc(name) {
+    const stop_name = name.replace(name_pole_id_regex, "").trim().replace(/,(?!\s)/g, ", ");
+    let proper_stop_name = stop_name.toLowerCase().replace(name_title_case_regex, (txt) => {
+        return txt.toUpperCase();
+    });
+    return proper_stop_name.replace(name_replacements_regex_en, (matched) => {
+        const name_replace_key = Object.keys(name_word_replacements_tc).find(
+            key => key.toLowerCase() === matched.toLowerCase()
+        );
+        return name_word_replacements_tc[name_replace_key] || matched;
+    });
 }
 function rte_input_change() {
     const rte_input = document.getElementById("rte_input").value;
@@ -32,16 +82,31 @@ function rte_input_change() {
         }
     }
 }
-function proper_stop_name(name) {
-    const regex = /\s\(([A-Z]{2}\d{3}[A-Z]?)\)$/i;
-    const match = name.match(regex);
-    let cleanName = name.replace(regex, "").trim();
-    let titleCase = cleanName.replace(/\w\S*/g, (txt) => {
-        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-    });
-    return [titleCase, match ? match[1] : ""];
+async function get_stop_name(stop_id) {
+    const label = `kmb_stop_name/${stop_id}`;
+    let data_dictionary = {};
+    let return_data;
+    if (localStorage.getItem("kmb_data_dictionary")) {
+        data_dictionary = JSON.parse(localStorage.getItem("kmb_data_dictionary"));
+    }
+    if (data_dictionary[label] && localStorage.getItem(label) && (Date.now() - data_dictionary[label]) < (7 * 86400000)) {
+        return_data = JSON.parse(localStorage.getItem(label));
+    } else {
+        stop_fetch = await (await fetch(`${api_base}/stop/${stop_id}`)).json();
+        stop_fetch_data = stop_fetch["data"];
+        return_data = [proper_stop_name_en(stop_fetch_data["name_en"]), proper_stop_name_tc(stop_fetch_data["name_tc"]), proper_stop_name_id(stop_fetch_data["name_en"]), stop_fetch_data["lat"], stop_fetch_data["long"]];
+        localStorage.setItem(label, JSON.stringify(return_data));
+        data_dictionary[label] = Date.now();
+        localStorage.setItem("kmb_data_dictionary", JSON.stringify(data_dictionary))
+    }
+    return return_data;
 }
 async function update() {
+    if (!document.getElementById("rte_options_select").value) {
+        document.getElementById("message_area").innerHTML = "Invalid route or direction, please input again.";
+        return;
+    }
+    document.getElementById("message_area").innerHTML = "Please wait";
     try {
         const rte_options_select = document.getElementById("rte_options_select");
         const bound = rte_options_select.value[0];
@@ -52,10 +117,10 @@ async function update() {
         const stop_data = await local_storage_item(`kmb_rte_stop_list/${route}/${bound}/${service_type}`, stop_data_url, 7);
         let stop_names = [];
         const stop_promises = stop_data["data"].map(async (i) => {
-            let i_stop_data = await local_storage_item(`kmb_stop/${i["stop"]}`, `${api_base}/stop/${i["stop"]}`, 7);
+            const stop_name_data = await get_stop_name(i["stop"]);
             return {
                 seq: Number(i["seq"]) - 1,
-                data: [i["stop"], proper_stop_name(i_stop_data["data"]["name_en"]), proper_stop_name(i_stop_data["data"]["name_tc"]), i_stop_data["data"]["lat"], i_stop_data["data"]["long"]]
+                data: [i["stop"], stop_name_data]
             };
         });
         const results = await Promise.all(stop_promises);
@@ -68,17 +133,19 @@ async function update() {
                 if (!eta_list[Number(i["seq"])-1]) {
                     eta_list[Number(i["seq"])-1] = [];
                 }
-                eta_list[Number(i["seq"])-1].push([i["eta"], i["rmk_en"], i["rmk_tc"]]);
+                eta_list[Number(i["seq"])-1].push([i["eta"], i["rmk_en"]]);
             }
         }
         to_display(eta_list, stop_data, bound, stop_names);
     } catch (error) {
         console.log(error);
+        document.getElementById("message_area").innerHTML = "An error occurred, please try again.";
         localStorage.removeItem("kmb_data_dictionary");
-        location.reload();
+        setup();
     }
 }
 async function to_display(eta_list, stop_data, bound, stop_names) {
+    document.getElementById("message_area").innerHTML = "";
     eta_data = eta_list;
     let results = "";
     for (let i = 0; i < stop_names.length; i++) {
@@ -89,7 +156,7 @@ async function to_display(eta_list, stop_data, bound, stop_names) {
         }
         results += `<div class='circle_${circ_colour}' onclick='stop_eta_update("${stop_names[i][0]}");'></div>`;
         results += `<div onclick='stop_click(${i});'>`;
-        results += `<p class='stop_disp_name'><span class='text_bold'>${i}</span> ${stop_names[i][1][0]} <span class='text_desc'>${stop_names[i][1][1]}</span></p>`;
+        results += `<p class='stop_disp_name'><span class='text_bold'>${i}</span> ${stop_names[i][1][0]} <span class='text_desc'>${stop_names[i][1][2]}</span></p>`;
         results += "</div>";
         results += "</div>";
     }
@@ -97,11 +164,11 @@ async function to_display(eta_list, stop_data, bound, stop_names) {
 }
 async function stop_eta_update(stop_id) {
     let dialog_results = "";
-    const stop_name_data = await local_storage_item(`kmb_stop/${stop_id}`, `${api_base}/stop/${stop_id}`, 7);
-    const stop_name = [stop_id, proper_stop_name(stop_name_data["data"]["name_en"]), proper_stop_name(stop_name_data["data"]["name_tc"]), stop_name_data["data"]["lat"], stop_name_data["data"]["long"]];
+    const stop_name_data = await get_stop_name(stop_id);
+    const stop_name = [stop_id, stop_name_data];
     dialog_results += `<span class='title_inline_block'>${stop_name[1][0]}</span> `;
-    dialog_results += `<span class='title_inline_block'>${stop_name[2][0]}</span> `;
-    dialog_results += `<span class='title_inline_block text_desc'>${stop_name[1][1]}</span>`;
+    dialog_results += `<span class='title_inline_block'>${stop_name[1][1]}</span> `;
+    dialog_results += `<span class='title_inline_block text_desc'>${stop_name[1][2]}</span>`;
     document.getElementById("stop_eta_disp_title").innerHTML = dialog_results;
     document.getElementById("stop_eta_disp_eta").replaceChildren();
     document.getElementById("stop_eta_disp_eta").innerHTML = "<p class='text_bold'>Please wait</p>";
@@ -121,11 +188,15 @@ async function find_terminus_index(route, bound, service_type) {
         return terminus_index;
     }
 }
+
+
+
+
 async function stop_eta_fetch(stop_id) {
     const eta_data = await (await fetch(`${api_base}/stop-eta/${stop_id}`)).json();
     let eta_presort = {};
     let eta_rd = {};
-    eta_data["data"].forEach((i) => {
+    for (const i of eta_data["data"]) {
         const rd_string = `${i["route"]}/${i["dir"]}`;
         const sub_data_id = `${i["route"]}/${i["dir"]}/${i["seq"]}`;
         const dest_string = proper_stop_name(i["dest_en"])[0];
@@ -143,17 +214,15 @@ async function stop_eta_fetch(stop_id) {
                     display_type = 3;
                 }
             } else {
-                find_terminus_index(i["route"], i["dir"], i["service_type"])
-                .then(response => {
-                    return response;
-                })
-                .then(data => {
-                    if (data === i["seq"]) {
+                try {
+                    const terminus_index = await find_terminus_index(i["route"], i["dir"], i["service_type"]);
+                    if (terminus_index === i["seq"]) {
                         display_type = 1;
                     }
-                });
+                } catch (e) {
+                    console.log(e);
+                }
             }
-            console.log(display_type);
             if (!eta_presort[sub_data_id]) {
                 const seq = i["seq"];
                 eta_presort[sub_data_id] = [display_type, [eta_list]];
@@ -166,13 +235,15 @@ async function stop_eta_fetch(stop_id) {
                 eta_rd[rd_string]["dest"].push(dest_string);
             }
         }
-    });
+    }
     const eta_sorted = Object.entries(eta_presort).sort((a, b) => {
         return a[1][0] - b[1][0];
     });
     return [eta_sorted, eta_rd];
 }
 function stop_eta_display(stop_id, [eta_sorted, eta_rd]) {
+    console.log(eta_sorted);
+    console.log(eta_rd);
     let results = "<table class='eta_data'>";
     for (const i of eta_sorted) {
         const sub_data = i[0].split("/");
@@ -188,9 +259,9 @@ function stop_eta_display(stop_id, [eta_sorted, eta_rd]) {
                 const eta_time = k[0];
                 const eta_time_mins = time_difference_format(eta_time);
                 const time_data = `<span class="text_bold">${eta_time_mins}</span> <span class="text_small">min</span>`;
-                eta_disp_content += `<td>${time_data}</td>`;
+                eta_disp_content += `<td width="100px">${time_data}</td>`;
             }
-            results += `<tr><td>${rte_dest_table}</td>${eta_disp_content}</tr>`;
+            results += `<tr><td width="100px">${rte_dest_table}</td>${eta_disp_content}</tr>`;
         } else if (display_type === 2) {
             results += `<tr><td>${rte_dest_table}</td><td colspan="3">${i[1][1][0][1]}</td></tr>`;
         } else {
@@ -279,4 +350,7 @@ async function local_storage_item(label, url, validity_period) {
         localStorage.setItem("kmb_data_dictionary", JSON.stringify(data_dictionary))
     }
     return return_data;
+}
+function proper_stop_name(name) {
+    return [proper_stop_name_en(name),  proper_stop_name_id(name)];
 }
